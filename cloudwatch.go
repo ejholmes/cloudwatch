103f01689ef382f7e174c320cdd4fc816e1e89e3
Eric J. Holmes <eric@ejholmes.net>
2015-11-25 05:36:59 +0700

    Add Group type.

--- cloudwatch.go
+++ cloudwatch.go
@@ -16,3 +16,35 @@ type client interface {
 	CreateLogStream(*cloudwatchlogs.CreateLogStreamInput) (*cloudwatchlogs.CreateLogStreamOutput, error)
 	GetLogEvents(*cloudwatchlogs.GetLogEventsInput) (*cloudwatchlogs.GetLogEventsOutput, error)
 }
+
+// Group wraps a log stream group and provides factory methods for creating
+// readers and writers for streams.
+type Group struct {
+	group  string
+	client *cloudwatchlogs.CloudWatchLogs
+}
+
+// NewGroup returns a new Group instance.
+func NewGroup(group string, client *cloudwatchlogs.CloudWatchLogs) *Group {
+	return &Group{
+		group:  group,
+		client: client,
+	}
+}
+
+// Create creates a log stream in the group and returns an io.Writer for it.
+func (g *Group) Create(stream string) (*Writer, error) {
+	if _, err := g.client.CreateLogStream(&cloudwatchlogs.CreateLogStreamInput{
+		LogGroupName:  &g.group,
+		LogStreamName: &stream,
+	}); err != nil {
+		return nil, err
+	}
+
+	return NewWriter(g.group, stream, g.client), nil
+}
+
+// Open returns an io.Reader to read from the log stream.
+func (g *Group) Open(stream string) (*Reader, error) {
+	return NewReader(g.group, stream, g.client), nil
+}
